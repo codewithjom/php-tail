4e29fa714a9b8e7671f4da2a8f1d518d48342d6a
Jom Dollesin <codewithjom@gmail.com>
2023-03-22 23:13:46 +0800

    feat: in the beginning...

---
+++ files/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.php", "./components/**/*.php", "./**/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
